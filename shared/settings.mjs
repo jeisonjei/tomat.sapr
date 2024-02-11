b03ad377fe2e755daadb0706a9e6ae619498f71d
jeisonj2019 <jeisonj2019@gmail.com>
2024-02-11 10:18:29 +0500

    tripLen increased

--- shared/settings.mjs
+++ shared/settings.mjs
@@ -1,6 +1,6 @@
 export class s{
     static tolerance = 0.02;
-    static tripLen = 1;
+    static tripLen = 10;
     static aspectRatio = 1;
     static setAspectRatio(width,height) {
         this.aspectRatio = height / width;
