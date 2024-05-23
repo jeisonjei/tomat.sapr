8a2565f28663f46d3dac9bc757a58f12b504d8bf
jeisonj2019 <jeisonj2019@gmail.com>
2024-05-23 21:08:32 +0500

    Добавлена заготовка службы 'database'

---
+++ services/database.js
@@ -0,0 +1,19 @@
+function initialize() {
+    var request = indexedDB.open('tomatDatabase');
+    
+}
+function list() {
+    
+}
+function create(obj) {
+    
+}
+function retrieve(id) {
+    
+}
+function update(id, obj) {
+    
+}
+function del(id) {
+    
+}
